8c4f4802f6705542818a320dd5159c45f5fc34ae
Sala D8 <CSalaD8@uniboyaca.edu.co>
2023-03-11 12:00:16 -0500

    CLASE 11/03/23

--- js/ejemplo.js
+++ js/ejemplo.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
   $(".acordeon li article").hide();
-  $(".acordeon li button").click(() => {
-    console.log($(this));
-  //  $("acordeon li article").show();
+  $(".acordeon li button").click((e) => {
+    var idBtn= e.target.id;
+    var idArticle = idBtn.replace('Btn', '')
+   $("#" + idArticle).toggle();
   });
 });
