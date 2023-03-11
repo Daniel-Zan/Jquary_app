5ab54ad49ddac085f685c5d6ddb96ee3a190d23b
Sala D8 <CSalaD8@uniboyaca.edu.co>
2023-03-11 11:13:03 -0500

    Oli

---
+++ js/ejemplo.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('.acordeon li article').hide();
+    $('.acordeon li button').click(()=>{
+        alert('El que cierre esta alerta es feo ;(!!!')
+    });
+    });
+
